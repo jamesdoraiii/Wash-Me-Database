4dd8a4e6fb5af9da9019aed850fe5ab0e82fd6cc
James Dora <jamesdoraiii@gmail.com>
2019-05-11 23:35:27 -0400

    review controller adjustment

--- controllers/reviewcontroller.js
+++ controllers/reviewcontroller.js
@@ -1,51 +1,49 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var sequelize = require('../db');
-var Review = sequelize.import('../models/review');
-
+var sequelize = require("../db");
+var Review = sequelize.import("../models/review");
 
 //This router creates a new review
 
-router.post('/create', (req, res) => {
-    const newReview = {
-        userId: req.user.id,
-        detailerId: req.body.detailerId,
-        reviewContent: req.body.reviewContent,
-        numberOfStars: req.body.numberOfStars
-    };
- 
-    Review.create(newReview)
-        .then(review => res.status(200).json(review))
-        .catch(err => res.status(500).json({ error: err }))
- })
+router.post("/create", (req, res) => {
+  const newReview = {
+    userId: req.user.id,
+    detailerId: req.body.detailerId,
+    reviewContent: req.body.reviewContent,
+    numberOfStars: req.body.numberOfStars
+  };
 
+  Review.create(newReview)
+    .then(review => res.status(200).json(review))
+    .catch(err => res.status(500).json({ error: err }));
+});
 
 //This router deletes a review by id
 
-router.delete('/deletereview/:id', (req, res) => {
-    Review.destroy({ where: { id: req.params.id} })
-        .then(deleted => res.status(200).json({
-            message: 'review succesfully deleted'
-        }))
-        .catch(err => res.status(500).json({ error : err }))
- })
-
+router.delete("/deletereview/:id", (req, res) => {
+  Review.destroy({ where: { id: req.params.id } })
+    .then(deleted =>
+      res.status(200).json({
+        message: "review succesfully deleted"
+      })
+    )
+    .catch(err => res.status(500).json({ error: err }));
+});
 
 //This router will find all reviews that a user has made
 
-router.get('/finduserreviews', (req, res) => {
-    Review.findAll({ where: { userId: req.user.id} })
-       .then(reviews => res.status(200).json(reviews))
-       .catch(err => res.status(500).json({ error: err }))
+router.get("/finduserreviews", (req, res) => {
+  Review.findAll({ where: { userId: req.user.id }, order: [['id', 'DESC']] })
+    .then(reviews => res.status(200).json(reviews))
+    .catch(err => res.status(500).json({ error: err }));
 });
-
 
 //This router will find all reviews made about a certain detailer
 
-router.get('/finddetailerreviews/:id', (req, res) => {
-    Review.findAll({ where: { detailerId: req.params.id} })
-       .then(reviews => res.status(200).json(reviews))
-       .catch(err => res.status(500).json({ error: err }))
+router.get("/finddetailerreviews/:id", (req, res) => {
+  Review.findAll({ where: { detailerId: req.params.id }, order: [['id', 'DESC']] })
+    .then(reviews => res.status(200).json(reviews))
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
